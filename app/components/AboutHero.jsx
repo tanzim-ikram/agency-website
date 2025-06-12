5cf70129ec80c0bbd5b541beaeeece3e53173931
Tanzim Ikram Sheikh <tanzim.ikram@gmail.com>
2025-06-12 19:34:45 +0600

    Refactor code structure for improved readability and maintainability

--- app/components/AboutHero.jsx
+++ app/components/AboutHero.jsx
@@ -5,7 +5,7 @@ import { Icon } from "@iconify/react";
 const AboutHero = () => {
   return (
     <div>
-      <section className="relative overflow-hidden bg-white w-full">
+      <section className="relative overflow-hidden bg-white w-full h-[1250px] sm:h-auto">
         {/* Background lines */}
         <Image
           src="/home/BG lines.png"
@@ -19,7 +19,7 @@ const AboutHero = () => {
           alt="BG Top"
           width={1440}
           height={186}
-          className="absolute inset-0 object-cover top-0 left-0 z-10 pointer-events-none"
+          className="absolute inset-0 object-cover top-0 left-0 z-20 pointer-events-none"
         />
 
         {/* Main content container */}
@@ -27,34 +27,70 @@ const AboutHero = () => {
           {/* Text Section wrapper with padding */}
           <div className="px-5 md:px-10 xl:px-[16%] pt-24 max-w-7xl mt-10">
             <h1
-              className="font-sen text-zinc-900 text-6xl font-normal leading-tight"
+              className="font-sen text-zinc-900 text-[36px] sm:text-6xl font-normal leading-[110%] text-left px-4 sm:px-6 md:px-8"
               style={{ lineHeight: "110%" }}
             >
-              Let's Get <br />
+              Let's Get
+              <br className="hidden sm:block" />
               <span className="text-zinc-500">To</span> Know
-              <Image
-                src="/about/Text-Img1.png"
-                alt="Handshake"
-                width={150}
-                height={80}
-                className="inline-block mx-2 rounded-full"
-              />
-              Each Other
+              <div className="inline-block mt-2 sm:mt-0">
+                <Image
+                  src="/about/Text-Img1.png"
+                  alt="Handshake"
+                  width={140}
+                  height={80}
+                  className="inline-block sm:mr-4 sm:ml-4 mr-3 ml-0 object-cover rounded-full sm:w-35 w-37.5 sm:h-16  h-12.5"
+                />
+                Each Other
+              </div>
             </h1>
             <p
-              className="font-figtree text-zinc-500 font-normal text-lg leading-relaxed mt-6 max-w-lg"
+              className="font-figtree text-zinc-500 font-normal text-lg leading-relaxed mt-6 max-w-lg px-4 sm:px-6 md:px-8"
               style={{ lineHeight: "160%" }}
             >
-              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;We
-              are a passionate team of developers, designers, and strategists
+              <span className="hidden md:inline">
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              </span>
+              We are a passionate team of developers, designers, and strategists
               committed to transforming businesses with cutting-edge technology.
               Our mission is to deliver scalable, secure, and data-driven
               solutions that empower businesses to thrive in a fast-changing
               digital world.
             </p>
 
-            {/* Recognized by */}
-            <div className="pt-60 relative">
+            {/* Clutch Rating Mobile*/}
+            <div className="relative md:hidden sm:absolute flex flex-col items-center justify-center w-full text-center z-30 gap-2 p-6">
+              <p className="text-lg text-black/60 font-figtree font-semibold">
+                RECOGNIZED BY:
+              </p>
+              <div className="text-yellow-400 text-xl flex justify-center gap-1 items-center">
+                {Array(5)
+                  .fill(0)
+                  .map((_, i) => (
+                    <Icon
+                      icon="tabler:star-filled"
+                      width={20}
+                      height={20}
+                      key={i}
+                    />
+                  ))}
+                <p className="text-gray-900 font-semibold text-lg ml-2">5.0</p>
+              </div>
+              <p className="mt-1 text-sm text-zinc-500 flex justify-center items-center">
+                From 80+ review from
+                <Image
+                  src="/home/clutch.png"
+                  alt="Clutch"
+                  width={18}
+                  height={18}
+                  className="mx-1 inline-block"
+                />
+                <span className="font-semibold text-zinc-500">Clutch</span>
+              </p>
+            </div>
+
+            {/* Recognized by Desktop*/}
+            <div className="pt-60 relative hidden md:block">
               <p className="text-lg text-black/60 font-figtree font-semibold mb-2">
                 RECOGNIZED BY:
               </p>
@@ -88,7 +124,7 @@ const AboutHero = () => {
           </div>
 
           {/* Overlay Image */}
-          <div className="absolute -z-10 -bottom-2 right-0">
+          <div className="hidden sm:block absolute -z-10 -bottom-2 right-0">
             <Image
               src="/about/BG Gr Bottom.svg"
               alt="BG Bottom"
@@ -97,19 +133,29 @@ const AboutHero = () => {
             />
           </div>
 
-          {/* Hero Image */}
-          <div className="absolute -z-10 -top-10 right-0">
+          {/* Hero Image Desktop*/}
+          <div className="hidden md:block absolute -z-10 top-0 right-0">
             <Image src="/about/BG.svg" alt="Hero" width={705} height={631} />
+          </div>
+          {/* Hero Image Mobile*/}
+          <div className="md:hidden absolute -z-10 -bottom-105 right-12">
+            <Image 
+            src="/about/BG2.svg" 
+            alt="Hero" 
+            width={700} 
+            height={500}
+            className="h-100 w-120 object-cover"
+            />
           </div>
         </div>
 
-        <div className="relative z-20 overflow-hidden bg-white">
+        <div className="absolute md:relative z-20 bottom-10 overflow-hidden">
           <Image
-            src="/about/HeroImage.png"
+            src="/about/HeroImage3.png"
             alt="Hero"
             width={1440}
             height={600}
-            className="w-full h-auto object-cover"
+            className="sm:w-full sm:h-auto h-70"
           />
         </div>
       </section>
--- app/components/ShortFooter.jsx
+++ app/components/ShortFooter.jsx
@@ -29,16 +29,16 @@ const ShortFooter = () => {
           {/* Wrap nav in a flex container centered */}
           <div className="flex w-full md:w-auto">
             <nav className="flex w-full md:w-auto justify-start md:justify-center gap-6 sm:gap-6 font-figtree font-normal leading-[160%] text-base sm:text-lg text-zinc-900 items-center text-center">
-              <a href="#" className="hover:underline">
+              <a href="/home" className="hover:underline">
                 Home
               </a>
-              <a href="#" className="hover:underline">
+              <a href="/services" className="hover:underline">
                 Services
               </a>
-              <a href="#" className="hover:underline">
+              <a href="/about" className="hover:underline">
                 About Us
               </a>
-              <a href="#" className="hover:underline">
+              <a href="/career" className="hover:underline">
                 Career
               </a>
             </nav>
